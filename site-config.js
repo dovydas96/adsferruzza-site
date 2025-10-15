c7050de7d5730e02684c65ba1b38e6b8eafa8033
Dovy <dovydas1996@gmail.com>
2025-10-15 18:07:07 +0100

    enable blogs

--- site-config.js
+++ site-config.js
@@ -1,3 +1,3 @@
 // Site-wide feature flags
 // Toggle to enable/disable the public blog listing quickly without removing data files
-window.BLOGS_ENABLED = false; // set to true when you want to show posts
+window.BLOGS_ENABLED = true; // set to true when you want to show posts
